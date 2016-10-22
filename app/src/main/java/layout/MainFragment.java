122371a2b59c0c5a90f3781c0e62f2a691cf6bd7
Jason Chandler <jason.chandlerp@gmail.com>
2016-10-22 01:05:26 -0400

    Initial Commit - Setup
    drawables and fragments added

---
+++ app/src/main/java/layout/MainFragment.java
@@ -0,0 +1,20 @@
+package layout;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jason.androidactionbar.R;
+
+
+public class MainFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/layout/TextFragment.java
@@ -0,0 +1,24 @@
+package layout;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jason.androidactionbar.R;
+
+
+public class TextFragment extends Fragment {
+    TextView text;
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.text_fragment, container, false);
+        text= (TextView) view.findViewById(R.id.txt);
+        String menu = getArguments().getString("Menu");
+        text.setText(menu);
+        return view;
+    }
+
+}
